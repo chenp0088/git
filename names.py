1b6269712c5e8fe8ae95b67e23c3042ff3b124a9
chenp0088 <pc008@sohu.com>
2018-08-09 21:56:01 +0800

    学习python文件

---
+++ names.py
@@ -0,0 +1,8 @@
+names=['petter','wangda','lishan']
+print (names[0])
+print (names[1])
+print (names[2])
+message=names[0].title()+" is a good boy"
+print (message)
+message=names[1].title()+" is a good girl"
+print (message)
