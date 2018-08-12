2c32172744550387824dc054b81fc4b66f57844a
chenp0088 <pc008@sohu.com>
2018-08-12 19:58:07 +0800

    弹出列表中任何位置处的元素

---
+++ moto_5.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# coding=utf-8
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop(0)
+print(motorcycles)
+print(popped_motorcycle)
