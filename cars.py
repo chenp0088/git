5d5bed31964b12e89d19f19c022c3970b68af1c5
chenp0088 <pc008@sohu.com>
2018-08-12 20:23:55 +0800

    使用sort()对列表进行永久排序

---
+++ cars.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# coding=utf-8
+cars = ['bmw','audi','toyota','subaru']
+cars.sort()
+print(cars)
---
+++ cars_1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# coding=utf-8
+cars = ['bmw','audi','toyota','subaru']
+cars.sort(reverse=True)
+print(cars)
