ec1d4acc4f8f2e67c1226cdf80a6c4bf974268c2
chenp0088 <pc008@sohu.com>
2018-10-11 09:55:19 +0800

    列表切片举例

---
+++ players_1.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+players=['charles','martina','micharl','florence','eli']
+print("print(players)")
+print("所有列表")
+print(players[1:3])
+print("从列表第２个元素开始到第３个元素")
+print(players[:4]) #从列表头开始
+print("从列表头开始")
+print(players[2:]) #从第３个元素到末尾的所有元素　
+print("从第３个元素到末尾的所有元素")
+print(players[-3:])#从最后三名输出
+print("从最后三名输出")
