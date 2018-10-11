e410a66a968998d3586c142919054dc4f5afcc73
chenp0088 <pc008@sohu.com>
2018-10-11 11:39:40 +0800

    遍历切片并打印

---
+++ player_2.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+players=['charles','martina','micharl','florence','eli']
+print("Here ar the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+
