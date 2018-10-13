10ae1d31daba9071b6a7809175f31b95d636c72d
chenp0088 <pc008@sohu.com>
2018-10-13 11:49:02 +0800

    p76 5-6 if-elif-else

---
+++ age.py
@@ -0,0 +1,13 @@
+age=40
+if age<2:
+    print("He is a baby.")
+elif 2<=age<4:
+    print("He is a child.")
+elif 4<=age<13:
+    print("He is a children.")
+elif 13<=age<20:
+    print("He is a teenager.")
+elif 20<=age<65:
+    print("He is an adult. ")
+elif age>65:
+    print("He is an old man")
