9c95623dc4337010f26d2d0bb0b1d5dd03d80b0c
chenp0088 <pc008@sohu.com>
2018-10-13 11:21:25 +0800

    p76 5-5 外星人if-elif-else用法

---
+++ alien_color_7.py
@@ -0,0 +1,10 @@
+alien_color=['green','yellow','red']
+alien='green'
+if alien=='green':
+    print("You get 5 points")
+elif alien=='yellow':
+    print("You get 10 points")
+elif alien=='red':
+    print("You get 15 points")
+else:
+    print("You get 0 points")
