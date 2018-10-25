847585a20fbb8cfccb40554b70a378367f4db276
chenp0088 <pc008@sohu.com>
2018-10-25 13:36:46 +0800

    字典存储类对象

---
+++ favorite.py
@@ -0,0 +1,4 @@
+favorite_languages = {
+        'jen':'python','sarch':'c','edward':'ruby','phil':'python',
+        }
+print("Sarch`s favorite language is "+ favorite_languages['sarch'].title()+".")
