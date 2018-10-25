3e8c525bb6294f573cb6e9175d6fc5e53e6bd137
chenp0088 <pc008@sohu.com>
2018-10-25 14:07:55 +0800

    p87 6-2

---
+++ names_1.py
@@ -0,0 +1,13 @@
+names ={
+        'name_1':'cp001','name_2':'cp002','name_3':'cp003','name_4':'cp004','name_5':'cp005'
+        }
+names['number_1'] = 1
+names['number_2'] = 2
+names['number_3'] = 3
+names['number_4'] = 4
+names['number_5'] = 5
+print(names['name_1'].title()+" favorite numbers "+str(names['number_1'])+".")
+print(names['name_2'].title()+" favorite numbers "+str(names['number_2'])+".")
+print(names['name_3'].title()+" favorite numbers "+str(names['number_3'])+".")
+print(names['name_4'].title()+" favorite numbers "+str(names['number_4'])+".")
+print(names['name_5'].title()+" favorite numbers "+str(names['number_5'])+".")
