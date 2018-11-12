11689a2f92bedf17b78cf67ae0e0144fb21a28d1
cp <pc008@sohu.com>
2018-11-12 13:57:41 +0800

    字典中关联一个列表遍历

---
+++ pizza.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding=utf-8
+pizza = {'crust':'thick','toppings':['mushrooms','extra cheese'],}
+print("You ordered a "+pizza['crust']+"-crust pizza "+"with the following toppings:")
+for topping in pizza['toppings']:
+    print("\t"+topping)
