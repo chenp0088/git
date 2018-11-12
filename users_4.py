9e3116d367658dec68ae3e9d27ab222d4047f7ca
cp <pc008@sohu.com>
2018-11-12 15:40:33 +0800

    p87 6-1

---
+++ users_4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+users = {
+    'first_name':'chen',
+    'last_name':'ping',
+    'age':'40',
+    'city':'taizhou'
+}
+print("\nFirst name: "+users['first_name'].title())
+print("\nLast name: "+users['last_name'].title())
+print("\nAge: "+users['age'].title())
+print("\nCity: "+users['city'].title())
