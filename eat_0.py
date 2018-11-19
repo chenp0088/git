5ef060a94fb0ed509e3a99588aa88928ca65fd1f
cp <pc008@sohu.com>
2018-11-19 11:37:45 +0800

    p104-7-2

---
+++ eat_0.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+number = input("What`s the number of the people? Please input the number:")
+number = int(number)
+if number >=8:
+    print("It has not table")
+else:
+    print("Please come in ,we have enough table")
